be391ac74e0904eed88995c564bb206a3860e0fd
Kauã S. Maia <kauamaia@yahoo.com.br>
2023-09-15 03:05:10 -0300

    Fix: Update task now does not create new task element html but really update it

--- front-end/main.js
+++ front-end/main.js
@@ -27,7 +27,6 @@ $(function() {
     }   
     
     function deleteTask(task){
-        console.log(task);
         task.remove()
     }
 
@@ -40,8 +39,15 @@ $(function() {
         
         try{
             if(name && due && category && priority && description){
-                checkExistentTask(name);
-                taskBuild(name, due, category,priority,description);            
+                let task = findTaskByName(name)
+                if(task){
+                    due = new Date(due);
+                    due = parseDate(due,2)
+                    newTaskString  = taskRebuild(task,name,due,category,priority,description)
+                    editTaskContent(name, newTaskString)
+                } else{
+                    taskBuild(name, due, category,priority,description);            
+                }
             } else{
                 alert("Please complete all fields");
             }   
@@ -50,14 +56,27 @@ $(function() {
         }               
     }
 
-    function checkExistentTask(name) {
+    function findTaskByName(name){
+        let val = null
+        $(".task").each(function() {
+            if($(this).attr('id') == name){               
+                val =  $(this)
+            }
+        })
+        return val
+    }
+
+    function editTaskContent(name, content) {
         
         $(".task").each(function() {
-            if($(this).attr('id') == name){                
-                deleteTask(this);
+            if($(this).attr('id') == name){               
+                $(this).html(content)
+                $(this).attr("id", name)
+
+                deleteToAll();
+                editToAll();
             }
-        })   
-        
+        })          
     }
 
     function taskBuild(name, due, category, priority, description){
@@ -87,18 +106,41 @@ $(function() {
                             </div>
                         </article>`
 
-
-        $("#task_list").append(new_task)
-        // addDeleteToButton("delete_"+ name);
+        $("#task_list").append(new_task)        
         deleteToAll();
         editToAll();
     }
-    
+    function taskRebuild(task,name, due, category, priority, description) {
+        task = `
+                    <div class="task_main">
+                        <h3>${name}</h3>
+                        <p>${description}</p>
+                    </div>                            
+                    <div class="task_info">
+                        <div class="task_details">
+                            <p>${due}</p>                          <p>${category}</p>
+                            <p>${priority}</p>
+                        </div>
+                        <div class="task_actions">
+                            <button class="edit_task" id="edit_${name}">
+                                <img src="./assets/pencil.png">
+                            </button>
+                            <button class="delete_task" id="delete_${name}">
+                                <img src="./assets/bin.png">
+                            </button>
+                        </div>
+                    </div>
+                `
+        
+        return task
+    }
+
+
     function editTask(task){
 
         let children = $(task).children().children()
 
-        console.log(children);
+        
 
         $("#new_task_name").val(children[0].innerText);        
         $("#new_task_description").val(children[1].innerText);
@@ -108,8 +150,7 @@ $(function() {
         newDue = newDue.split('/');
         newDue = new Date(newDue[2]+"-"+newDue[1]+"-"+newDue[0]);
         
-        
-        console.log(newDue);
+    
         $("#new_task_due").val(parseDate(newDue,1));
 
 
